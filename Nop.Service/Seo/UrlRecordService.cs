83f6d633c6e14b52a610733ece7b707d4c53949f
Adam M <whiskypo082@gmail.com>
2022-04-25 15:57:57 -0500

    Adding Slug Url for Seo

--- Nop.Service/Seo/UrlRecordService.cs
+++ Nop.Service/Seo/UrlRecordService.cs
@@ -10,19 +10,92 @@ namespace Nop.Service.Seo
 {
     public class UrlRecordService : IUrlRecordService
     {
-        public Task<string> GetSeNameAsync<T>(T entity) where T : BaseEntity, ISlugSupported
+        public Task<string> GetSeNameAsync<T>(T entity, int? languageId = null) where T : BaseEntity, ISlugSupported
         {
-            throw new NotImplementedException();
-        }
+            bool returnDefaultValue = true;
 
-        public Task<string> GetSeNameAsync<T>(int entityId, string entityName)
+            if (entity == null)
+            {
+                throw new ArgumentException(nameof(entity));    
+            }
+
+            var entityName = entity.GetType().Name;
+
+            return await GetSeNameAsync(entity.Id, entityName ?? (await _workContext.GetWorkingLanguageAsync()).Id, returnDefaultValue);
+        }
+        public async Task<string> GetSeNameAsync<T>(int entityId, string entityName)
         {
-            throw new NotImplementedException();
+            var result = string.Empty;
+            var loadLocalizedValue = true;
+            var returnDefaultValue = true;
+
+            //localized value 
+            if (loadLocalizedValue)
+            {
+                result = await GetActiveSlugAsync(entityId, entityName);
+            }
+
+            if (string.IsNullOrEmpty(result) && returnDefaultValue)
+            {
+                result = await GetActiveSlugAsync(entityId, entityName); 
+            }
+
+            return result;
         }
 
         public Task<string> GetSeNameAsync(string name)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(name))
+            {
+                return Task.FromResult(name); 
+            }
+
+            var okChars = "asbcdefghijklmnopqrstuvwxyz1234567890 _-";
+            name = name.Trim().ToLowerInvariant();
+
+            if (convertNonWesterenChars && _seoCharacterTable == null)
+            {
+                InitializeSeoCharacterTable(); 
+            }
+
+            var sb = new StringBuilder();
+            foreach (var c in name.ToCharArray())
+            {
+                var c2 = c.ToString();
+                if (convertNonWesternChars)
+                {
+                    if (_seoCharacterTable?.ContainsKey(c2) ?? false)
+                    {
+                        c2 = _seoCharacterTable[c2].ToLowerInvariant();
+                    }
+
+                    if (allowUnicodeCharsInUrls)
+                    {
+                        if (char.IsLetterOrDigit(c) || okChars.Contains(c2))
+                        {
+                            sb.Append(c2);
+                        }
+                        else if (okChars.Contains(c2))  
+                        {
+                            sb.Append(c2); 
+                        }
+                    }
+
+                    var name2 = sb.ToString();
+                    name2 = name2.Replace(" ", "-"); 
+                    while(name2.Contains("--"))
+                    {
+                        name2 = name2.Replace("--", "-"); 
+                    }
+                    while (name2.Contains("__"))
+                    {
+                        name2 = name2.Replace("__", "_"); 
+                    }
+
+                    return Task.FromResult(name2);
+                }
+            }
         }
+
     }
 }
